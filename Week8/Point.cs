ef7fa25bab200c86f23990cf4961ce27ab5449bd
ihnatovcharenko25-gif <ihnat.ovcharenko.25@kse.org.ua>
2026-03-28 17:19:45 +0200

    Stage 1

---
+++ Week8/Point.cs
@@ -0,0 +1,18 @@
+﻿namespace Week8;
+
+public struct Point
+{
+    public int X { get; private set; } 
+    public int Y { get; private set; }
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"{X}, {Y}");
+    }
+}
---
+++ Week8/PointRef.cs
@@ -0,0 +1,18 @@
+﻿namespace Week8;
+
+public class PointRef
+{
+    public int X { get; private set; } 
+    public int Y { get; private set; }
+
+    public PointRef(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"{X}, {Y}");
+    }
+}
