1d3dcb396fe73955e974b264bed18e2110f152fd
ihnatovcharenko25-gif <ihnat.ovcharenko.25@kse.org.ua>
2026-03-28 17:36:59 +0200

    Stage 1

---
+++ Week8/DataItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Week8;
+
+public class DataItem
+{
+    public string Id { get; set; } = string.Empty;
+    public byte[] Payload { get; set; } = Array.Empty<byte>();
+
+    public int GetSize() => Payload.Length;
+}
