c47bbaa3777b6d7e65b6f7874c323096a21e1312
ihnatovcharenko25-gif <ihnat.ovcharenko.25@kse.org.ua>
2026-03-28 18:02:55 +0200

    stage 3

---
+++ Week8/Program.cs
@@ -0,0 +1,22 @@
+﻿using Week8;
+
+var container = new DataContainer<DataItem>();
+for (int i = 0; i < 10000; i++)
+{
+    var item = new DataItem
+    {
+        Id = $"item-{i}",
+        Payload = new byte[1024]
+    };
+// TODO: add item to container
+container.AddItem(item);
+}
+Console.WriteLine($"Total size before GC: {container.GetTotalSize()} bytes");
+
+// TODO: call GC.Collect()
+GC.Collect();
+Console.WriteLine("GC.Collect() was called.");
+Console.WriteLine($"Total size after GC: {container.GetTotalSize()} bytes");
+Console.WriteLine($"We still have references to elements in stack, because of container instance");
+
+//We still have references to elements in stack, because of container instance, so GC can't delete it from heap
